f1e5ed21473b31fbb730d97b03282627caa5b680
daviddmr <david.dmr@gmail.com>
2019-09-12 15:05:09 -0300

    Creating EpisodeDetailActivity

---
+++ app/src/main/java/com/example/mobile_take_home/EpisodeDetailActivity.java
@@ -0,0 +1,20 @@
+package com.example.mobile_take_home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EpisodeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_episode_detail);
+        setupComponents();
+    }
+
+    private void setupComponents() {
+
+    }
+
+}
