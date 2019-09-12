cfb7717795f805471760c279054d91e35763a433
daviddmr <david.dmr@gmail.com>
2019-09-12 13:17:27 -0300

    Creating a Planet model class

---
+++ app/src/main/java/com/example/mobile_take_home/model/Planet.java
@@ -0,0 +1,25 @@
+package com.example.mobile_take_home.model;
+
+public class Planet {
+
+    private String name;
+
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
