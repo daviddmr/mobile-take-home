78de96b00ca44c0f778f8be243586d557ee3e7c0
daviddmr <david.dmr@gmail.com>
2019-09-12 13:03:46 -0300

    Creating a Episode model class

---
+++ app/src/main/java/com/example/mobile_take_home/model/Episode.java
@@ -0,0 +1,77 @@
+package com.example.mobile_take_home.model;
+
+import java.util.ArrayList;
+
+public class Episode {
+
+    private long id;
+
+    private String name;
+
+    private String episode;
+
+    private String url;
+
+    private String created;
+
+    private String airDate;
+
+    private ArrayList<String> characters;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEpisode() {
+        return episode;
+    }
+
+    public void setEpisode(String episode) {
+        this.episode = episode;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getAirDate() {
+        return airDate;
+    }
+
+    public void setAirDate(String airDate) {
+        this.airDate = airDate;
+    }
+
+    public ArrayList<String> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(ArrayList<String> characters) {
+        this.characters = characters;
+    }
+
+}
