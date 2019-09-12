1ae73a095cdba3120223e8f46440038ece2dc405
daviddmr <david.dmr@gmail.com>
2019-09-12 15:25:08 -0300

    Setting up the recyclerview on EpisodeDetailActivity

--- app/src/main/java/com/example/mobile_take_home/EpisodeDetailActivity.java
+++ app/src/main/java/com/example/mobile_take_home/EpisodeDetailActivity.java
@@ -1,10 +1,24 @@
 package com.example.mobile_take_home;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.mobile_take_home.model.Character;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class EpisodeDetailActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    private CharactersAdapter adapter;
+    private ArrayList<Character> characterList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +28,28 @@ public class EpisodeDetailActivity extends AppCompatActivity {
     }
 
     private void setupComponents() {
+        progressBar = findViewById(R.id.pb);
+        RecyclerView rvCharacters = findViewById(R.id.rv_characters);
+        rvCharacters.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new GridLayoutManager(this, 2);
+        rvCharacters.setLayoutManager(linearLayoutManager);
 
+        adapter = new CharactersAdapter(this, characterList, characterClickListener());
+        rvCharacters.setAdapter(adapter);
     }
 
+    private void updateList(ArrayList<Character> characters) {
+        int lastIndex = characterList.size();
+        characterList.addAll(characters);
+        adapter.notifyItemRangeInserted(lastIndex, characters.size());
+    }
+
+    private View.OnClickListener characterClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = view.getId();
+            }
+        };
+    }
 }
