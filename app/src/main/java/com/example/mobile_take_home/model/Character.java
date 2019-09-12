072a5c7a5f7dce544d4033804923ae4eba3df14d
daviddmr <david.dmr@gmail.com>
2019-09-12 16:05:42 -0300

    Implementing Parcelable in Character and Planet model classes

--- app/src/main/java/com/example/mobile_take_home/model/Character.java
+++ app/src/main/java/com/example/mobile_take_home/model/Character.java
@@ -1,8 +1,11 @@
 package com.example.mobile_take_home.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class Character {
+public class Character implements Parcelable {
 
     private long id;
 
@@ -124,4 +127,54 @@ public class Character {
         this.created = created;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(this.id);
+        dest.writeString(this.name);
+        dest.writeString(this.status);
+        dest.writeString(this.species);
+        dest.writeString(this.type);
+        dest.writeString(this.gender);
+        dest.writeParcelable(this.origin, flags);
+        dest.writeParcelable(this.location, flags);
+        dest.writeString(this.image);
+        dest.writeStringList(this.episode);
+        dest.writeString(this.url);
+        dest.writeString(this.created);
+    }
+
+    public Character() {
+    }
+
+    protected Character(Parcel in) {
+        this.id = in.readLong();
+        this.name = in.readString();
+        this.status = in.readString();
+        this.species = in.readString();
+        this.type = in.readString();
+        this.gender = in.readString();
+        this.origin = in.readParcelable(Planet.class.getClassLoader());
+        this.location = in.readParcelable(Planet.class.getClassLoader());
+        this.image = in.readString();
+        this.episode = in.createStringArrayList();
+        this.url = in.readString();
+        this.created = in.readString();
+    }
+
+    public static final Parcelable.Creator<Character> CREATOR = new Parcelable.Creator<Character>() {
+        @Override
+        public Character createFromParcel(Parcel source) {
+            return new Character(source);
+        }
+
+        @Override
+        public Character[] newArray(int size) {
+            return new Character[size];
+        }
+    };
 }
--- app/src/main/java/com/example/mobile_take_home/model/Planet.java
+++ app/src/main/java/com/example/mobile_take_home/model/Planet.java
@@ -1,6 +1,9 @@
 package com.example.mobile_take_home.model;
 
-public class Planet {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Planet implements Parcelable {
 
     private String name;
 
@@ -22,4 +25,34 @@ public class Planet {
         this.url = url;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.url);
+    }
+
+    public Planet() {
+    }
+
+    protected Planet(Parcel in) {
+        this.name = in.readString();
+        this.url = in.readString();
+    }
+
+    public static final Parcelable.Creator<Planet> CREATOR = new Parcelable.Creator<Planet>() {
+        @Override
+        public Planet createFromParcel(Parcel source) {
+            return new Planet(source);
+        }
+
+        @Override
+        public Planet[] newArray(int size) {
+            return new Planet[size];
+        }
+    };
 }
