7e96cc049dad0bccd18b0354d4b2c8d48af37502
daviddmr <david.dmr@gmail.com>
2019-09-13 05:07:54 -0300

    Adjustment on MainActivity layout

--- app/src/main/java/com/example/mobile_take_home/adapter/EpisodesAdapter.java
+++ app/src/main/java/com/example/mobile_take_home/adapter/EpisodesAdapter.java
@@ -1,6 +1,9 @@
 package com.example.mobile_take_home.adapter;
 
 import android.content.Context;
+import android.os.Build;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +20,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class EpisodesAdapter extends RecyclerView.Adapter<EpisodesAdapter.EpisodeViewHolder> {
 
+    private Context context;
     private LayoutInflater layoutInflater;
     private List<Episode> episodeList;
     private View.OnClickListener listener;
 
     public EpisodesAdapter(Context context, List<Episode> episodeList, View.OnClickListener listener) {
+        this.context = context;
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.listener = listener;
         this.episodeList = episodeList;
@@ -40,9 +45,11 @@ public class EpisodesAdapter extends RecyclerView.Adapter<EpisodesAdapter.Episod
         holder.mainLayout.setOnClickListener(listener);
         holder.mainLayout.setId(position);
 
-        holder.tvName.setText(episodeList.get(position).getName());
-        holder.tvEpisode.setText(episodeList.get(position).getEpisode());
-        holder.tvAirDate.setText(episodeList.get(position).getAirDate());
+        final Episode episode = episodeList.get(position);
+
+        holder.tvName.setText(String.format(context.getString(R.string.name_episode_label), episode.getName()));
+        holder.tvEpisode.setText(String.format(context.getString(R.string.number_episode_label), episode.getEpisode().substring(1, 3), episode.getEpisode().substring(4, 6)));
+        holder.tvAirDate.setText(String.format(context.getString(R.string.air_date_episode_label), episode.getAirDate()));
     }
 
     @Override
